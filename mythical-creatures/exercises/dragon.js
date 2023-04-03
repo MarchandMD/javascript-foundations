43afe232cabe32f816738bbb730290306be27a21
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-04-02 20:31:39 -0600

    update gitignore

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,36 @@
+var createDragon = (name, rider, temperament) => {
+    var timesEaten = 0
+    var hungry = true
+    return { name, rider, temperament, timesEaten, hungry }
+}
 
+var greetRider = (dragon) => {
+    return `Hi, ${dragon.rider}!`
+}
+
+var eat = (dragon) => {
+    let timesEaten = dragon.timesEaten
+    timesEaten++
+    if (timesEaten >= 3) {
+        dragon.hungry = false
+    }
+    let hungry = dragon.hungry
+    return { timesEaten, hungry }
+}
+
+var findFireBreathers = (array) => {
+    let arrayOfFireBreathers = []
+    for (i = 0; i < array.length; i++) {
+        if (array[i].temperament === "aggressive") {
+            arrayOfFireBreathers.push(array[i])
+        }
+    }
+    return arrayOfFireBreathers
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+    createDragon,
+    greetRider,
+    eat,
+    findFireBreathers,
 }
