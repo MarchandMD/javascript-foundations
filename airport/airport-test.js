e619a9071767c65f1526ce56c62c2a87e44d7c41
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-04-04 14:08:28 -0600

    Completes airport testing

--- airport/airport-test.js
+++ airport/airport-test.js
@@ -2,7 +2,7 @@ var assert = require('chai').assert;
 var { createAirport, welcomeGuests, landPlanes, checkAirlineLocations } = require('./airport');
 
 describe('Airport', function() {
-  it.skip('should create an airport', function() {
+  it('should create an airport', function() {
     var airport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
 
     assert.equal(airport.name, 'Denver International Airport');
@@ -10,11 +10,11 @@ describe('Airport', function() {
     assert.equal(airport.airlines[0], 'United');
   });
 
-  it.skip('should welcome people to the airport', function() {
+  it('should welcome people to the airport', function() {
     var denverAirport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
     var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
 
-    
+
     var denverWelcome = welcomeGuests(denverAirport);
     var sanDiegoWelcome = welcomeGuests(sanDiegoAirport);
 
@@ -22,7 +22,7 @@ describe('Airport', function() {
     assert.equal(sanDiegoWelcome, 'Welcome to San Diego International Airport!');
   });
 
-  it.skip('should keep track of open gates', function() {
+  it('should keep track of open gates', function() {
     var bakersfieldAirport = createAirport('Meadows Field Airport', ['United', 'American'], 12);
     var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
 
@@ -33,26 +33,26 @@ describe('Airport', function() {
     assert.equal(sanDiegoAirport.availableGates, 46);
   });
 
-  it.skip('should not be able to occupy more gates than available', function() {
-    var columbusAiport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
+  it('should not be able to occupy more gates than available', function() {
+    var columbusAirport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
 
-    var occupiedGates1 = landPlanes(columbusAiport, 22);
+    var occupiedGates1 = landPlanes(columbusAirport, 22);
 
-    assert.equal(columbusAiport.availableGates, 2);
+    assert.equal(columbusAirport.availableGates, 2);
     assert.equal(occupiedGates1, 'Success! Current availability is 2.')
 
-    var occupiedGates2 = landPlanes(columbusAiport, 3);
+    var occupiedGates2 = landPlanes(columbusAirport, 3);
 
-    assert.equal(columbusAiport.availableGates, 0);
+    assert.equal(columbusAirport.availableGates, 0);
     assert.equal(occupiedGates2, 'Oh no! Not enough gates available. Current overflow is 1.')
   });
 
-  it.skip('should be able to tell you where an airline flies to', function() {
-    var columbusAiport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
+  it('should be able to tell you where an airline flies to', function() {
+    var columbusAirport = createAirport('John Glenn Airport', ['Southwest', 'Frontier'], 24);
     var bakersfieldAirport = createAirport('Meadows Field Airport', ['United', 'American'], 12);
     var sanDiegoAirport = createAirport('San Diego International Airport', ['Frontier', 'American'], 48);
     var denverAirport = createAirport('Denver International Airport', ['United', 'Southwest', 'Delta'], 144);
-    var allAirports = [columbusAiport, bakersfieldAirport, sanDiegoAirport, denverAirport];
+    var allAirports = [columbusAirport, bakersfieldAirport, sanDiegoAirport, denverAirport];
 
     var southwestCarriers = checkAirlineLocations(allAirports, 'Southwest');
     var unitedCarriers = checkAirlineLocations(allAirports, 'United');
--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,32 @@
+var createAirport = (name, airlines, availableGates) => {
+    return { name, availableGates, airlines }
+}
 
+var welcomeGuests = (airport) => {
+    return `Welcome to ${airport.name}!`
+}
 
+var landPlanes = (airport, incomingFlights) => {
+    if (airport.availableGates - incomingFlights <= 0) {
+        var currentOverflow = incomingFlights - airport.availableGates
+        airport.availableGates = 0
+        return `Oh no! Not enough gates available. Current overflow is ${currentOverflow}.`
+    } else {
+        airport.availableGates = airport.availableGates - incomingFlights
+        return `Success! Current availability is ${airport.availableGates}.`
+    }
+}
 
-module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
-};
+var checkAirlineLocations = (allAirports, airline) => {
+    let locations = allAirports.filter((airport) =>
+        airport.airlines.includes(airline)
+    )
+    return locations.flatMap(location => location.name )
+}
+
+module.exports = {
+    createAirport,
+    welcomeGuests,
+    landPlanes,
+    checkAirlineLocations,
+}
